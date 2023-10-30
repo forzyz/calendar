5cc3a19ecc32acab4d95d5eb13983f24c38116f1
Ruslan Telitsyn <zimpuytel@gmail.com>
2023-10-30 10:58:26 +0200

    delete modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,13 @@
 import Calendar from "./Calendar";
 import "./App.css";
-import AddModal from "./components/AddModal";
 
-// Bootstrap CSS
-import "bootstrap/dist/css/bootstrap.min.css";
-// Bootstrap Bundle JS
-import "bootstrap/dist/js/bootstrap.bundle.min";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <>
       <Calendar />
-    </>
+    </> 
   );
 }
 
